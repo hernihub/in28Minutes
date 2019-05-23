86d84ecf605af5bac263c7a14b367d84a6c6ad05
hernihub <hernan_quevedo@yahoo.com>
2019-05-23 14:07:40 +1000

    initial commit

---
+++ src/main/java/com/in28minutes/rest/webservices/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package com.in28minutes.rest.webservices.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * 
+ * @author Hernán Camilo
+ *
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+	
+	@Bean
+    public Docket apiDocket() {
+        return new Docket(DocumentationType.SWAGGER_2);
+	}
+}
